f19f287841b6137e9591fb76906353053319cea5
Waxzoman <omar.morsy.yakout@gmail.com>
2024-01-24 01:06:46 +0100

    Created utils, avatar component and configured codegen to generate types

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,7 +1,11 @@
 import { Popover, Button } from 'antd'
 import React from 'react'
+import CustomAvatar from '../custom-avatar'
+import { useGetIdentity } from '@refinedev/core'
+import {User} from '@/graphql/schema.types'
 
 const CurrentUser = () => {
+  const {data: user} = useGetIdentity<User>()
   return (
         <> 
           <Popover
@@ -10,7 +14,12 @@ const CurrentUser = () => {
               overlayInnerStyle={{padding: 0}}
               overlayStyle={{zIndex: 999}}
           >
-            TEST
+              <CustomAvatar
+                  name={user?.name}
+                  src={user?.avatarUrl}
+                  size="default"
+                  style={{cursor: 'pointer'}}
+              />
           </Popover>  
         </>
   )
