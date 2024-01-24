3e590b1a56ce1929a23c53870b00e570a013b1ff
Waxzoman <omar.morsy.yakout@gmail.com>
2024-01-24 02:35:12 +0100

    Created resources to be displayed on the banner

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import { App as AntdApp } from "antd";
 
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
+import { resources } from "./config/resources";
 
 
 
@@ -32,6 +33,7 @@ function App() {
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 authProvider={authProvider}
+                resources={resources}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
