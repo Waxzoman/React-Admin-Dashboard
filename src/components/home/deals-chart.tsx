cbca0b2850149fcdc8c2f43ac846543b4410b787
Waxzoman <omar.morsy.yakout@gmail.com>
2024-01-26 23:24:46 +0100

    configured the chart in AreaConfig, passed filters to uselist hook

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -6,10 +6,17 @@ import { Area, AreaConfig } from '@ant-design/plots'
 import { useList } from '@refinedev/core'
 import { DASHBOARD_DEALS_CHART_QUERY } from '@/graphql/queries'
 import { mapDealsData } from '@/utilities/helpers'
+import { GetFieldsFromList } from '@refinedev/nestjs-query'
+import { DashboardDealsChartQuery } from '@/graphql/types'
 
 const Dealschart = () => {
-  const { data } = useList({
+  const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
     resource: 'dealStages',
+    filters: [
+      {
+        field: 'title', operator: 'in', value: ['WON', 'LOST']
+      }
+    ],
     meta: {
       gqlQuery: DASHBOARD_DEALS_CHART_QUERY
     }
@@ -22,7 +29,31 @@ const Dealschart = () => {
   const config: AreaConfig = {
     data: dealData,
     xField: 'timeText',
-    yField: 'value'
+    yField: 'value',
+    isStack: false,
+    seriesField: 'state',
+    animation: true,
+    startOnZero: false,
+    smooth: true,
+    legend: {
+      offsetY: -6,
+    },
+    yAxis: {
+      tickCount: 4,
+      label: {
+        formatter: (v: string) => {
+          return `$${Number(v) /1000}k`
+        }
+      }
+    },
+    tooltip: {
+      formatter: (data) => {
+        return {
+          name: data.state,
+          value: `$${Number(data.value) /1000}k`
+        }
+      }
+    }
   };
 
   return (
