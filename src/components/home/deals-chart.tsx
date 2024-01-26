15180f39a2c05a4d3ff9c48301364f85e23c3c22
Waxzoman <omar.morsy.yakout@gmail.com>
2024-01-26 23:12:07 +0100

    Passed mapDealsData from utils to deals-chart useMemo to provide data in AreaConfig

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -3,12 +3,28 @@ import { Card } from 'antd'
 import React from 'react'
 import { Text } from '../text'
 import { Area, AreaConfig } from '@ant-design/plots'
+import { useList } from '@refinedev/core'
+import { DASHBOARD_DEALS_CHART_QUERY } from '@/graphql/queries'
+import { mapDealsData } from '@/utilities/helpers'
 
 const Dealschart = () => {
-  const config: AreaConfig = {
-    data: []
-  };
+  const { data } = useList({
+    resource: 'dealStages',
+    meta: {
+      gqlQuery: DASHBOARD_DEALS_CHART_QUERY
+    }
+  });
+
+  const dealData = React.useMemo(() => {
+    return mapDealsData(data?.data);
+  }, [data?.data])
   
+  const config: AreaConfig = {
+    data: dealData,
+    xField: 'timeText',
+    yField: 'value'
+  };
+
   return (
     <Card
       style={{ height: '100%'}}
