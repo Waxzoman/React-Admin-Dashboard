ed0a1722d519520d855028bd13c53cf0334aa88c
Waxzoman <omar.morsy.yakout@gmail.com>
2024-02-01 14:40:35 +0100

    Created edit forum for companies

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,8 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
 import Create from "./pages/company/create";
+import Edit from "./pages/company/edit";
+import EditPage from "./pages/company/edit";
 
 
 
@@ -62,6 +64,7 @@ function App() {
                   <Route path="/companies"  >
                     <Route index element={<CompanyList />} />
                     <Route path="new" element={<Create/> } />
+                    <Route path="edit/:id" element={<EditPage/> } />
                   </Route>
                 </Route>
                 </Routes>
