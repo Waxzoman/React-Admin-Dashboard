cb317aa5f46cbec3b12ab7e65a979c0c33f008db
Waxzoman <omar.morsy.yakout@gmail.com>
2024-02-02 23:19:15 +0100

    finished initial styling of KanbanColum with mock data

--- src/components/tasks/kanban/column.tsx
+++ src/components/tasks/kanban/column.tsx
@@ -1,17 +1,77 @@
+import { Text } from '@/components/text'
+import { PlusOutlined } from '@ant-design/icons'
 import { useDroppable } from '@dnd-kit/core'
-import React from 'react'
+import { Badge, Button, Space } from 'antd'
+import React, { Children } from 'react'
 
 const KanbanColumn = () => {
     const { isOver, setNodeRef, active } = useDroppable({
       id: '',
       data: ''
-  })
+  });
   
+  const count =0;
+  
+  const description = 'Description';
+  const title = 'title';
+  
+  const onAddClickHandler = ({children}: React.PropsWithChildren) => {}
+    
   return (
     <div
-        ref={}
+        ref={setNodeRef}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',  
+          padding: '0 16px'
+        }}
     >
-        
+        <div style={{ padding: '12px'}}>
+            <Space
+                style={{width: '100%', justifyContent: 'space-between'}}
+            >
+                <Space>
+                    <Text
+                        ellipsis={{tooltip: title}}
+                        size='xs'
+                        strong
+                        style={{
+                            textTransform: 'uppercase',
+                            whiteSpace: 'nowrap',
+                        }}
+                    >
+                        {title}
+                    </Text>
+                      {!!count && <Badge count={count} color='cyan' />}
+                </Space>
+                <Button 
+                    shape='circle'
+                    icon={<PlusOutlined />}
+                    onClick={onAddClickHandler}
+                />
+            </Space>
+            {description}
+        </div>
+        <div
+              style={{
+                flex: 1,
+                overflowY: active ? 'unset' : 'scroll',
+                border: '2px dashed transparent',
+                borderColor: isOver ? '#000040' : 'transparent',
+                borderRadius: '4px'
+            }}
+        >
+            <div
+                  style={{
+                    marginTop: '12px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '8px'
+                }}
+            >
+                {children}
+            </div>
+        </div>
     </div>
   )
 }
