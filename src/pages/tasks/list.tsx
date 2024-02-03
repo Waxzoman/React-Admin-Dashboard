bd13de3fb9c974a28993bdfd1865bf8c333d8368
Waxzoman <omar.morsy.yakout@gmail.com>
2024-02-03 01:58:40 +0100

    extracted props tasks and stages props from uselist hooks and combined in taskstages fn with useMemo

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -1,9 +1,71 @@
 import { KanbanBoardContainer, KanbanBoard } from '@/components/tasks/kanban/board'
 import KanbanColumn from '@/components/tasks/kanban/column'
 import KanbanItem from '@/components/tasks/kanban/item'
+import { TASKS_QUERY, TASK_STAGES_QUERY } from '@/graphql/queries'
+import { TaskStage } from '@/graphql/schema.types'
+import { useList } from '@refinedev/core'
 import React from 'react'
 
 const List = () => {
+  const { data: stages, isLoading: isLoadingStages} = useList({
+    resource: 'taskStages',
+      filters: [
+        {
+            field: 'title',
+            operator: 'in',
+            value: ['TODO', 'IN PROGRESS', 'IN REVIEW', 'DONE']
+        }
+    ],
+    sorters: [
+        {
+            field: 'createdAt',
+            order: 'asc'
+        }
+    ],
+    meta: {
+        gqlQuery: TASK_STAGES_QUERY
+    }
+  });
+    
+  const { data: tasks, isLoading: isLoadingTasks } = useList({
+      resource: 'tasks',
+      sorters: [
+          {
+              field: 'dueDate',
+              order: 'asc',
+          }
+      ],
+      queryOptions: {
+        enabled: !!stages,
+      },
+      pagination: {
+          mode: 'off'
+      },
+      meta: {
+        gqlQuery: TASKS_QUERY
+      },
+  });
+
+  const taskStages = React.useMemo(()=>{
+    if(!tasks?.data || !stages?.data){
+        return {
+            unnasignedStage: [],
+            stages: []
+        }
+    }
+    
+    const unnasignedStage = tasks.data.filter((task)=> task.stageId === null);
+    const grouped: TaskStage[] = stages.data.map((stage)=>({
+        ...stage,
+        tasks: tasks.data.filter((task)=>task?.stageId.toString()=== stage.id)
+    }))
+      
+    return {
+        unnasignedStage,
+        columns: grouped
+    }
+  }, [stages, tasks])
+
   return (
     <>
         <KanbanBoardContainer>
